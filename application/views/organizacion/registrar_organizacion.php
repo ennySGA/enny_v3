f193ad05df4399b26c194cb4b35ae2f468c85402
Enny <ennySGA@gmail.com>
2013-07-03 16:28:07 -0500

    enny_v3

--- application/views/organizacion/registrar_organizacion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$is_logged_in=$this->session->userdata('is_logged_in');
-	if(!isset($is_logged_in)|| $is_logged_in!=TRUE){
-			redirect('login');
-	}
-$this->load->view('organizacion/form_organizacion.php')
-?>
